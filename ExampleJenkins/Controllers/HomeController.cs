9186f94c6b87015bb5bfc81cadf8c6ddc9ebb110
kdinh9 <merikakun@gmail.com>
2016-09-12 01:12:49 +0700

    dasdasdas

--- ExampleJenkins/Controllers/HomeController.cs
+++ ExampleJenkins/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace ExampleJenkins.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        // GET: Home /
         public ActionResult Index()
         {
             return View();
